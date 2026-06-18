61bb2607ff799ec50c52051e3cce069945abfa23
YingZhiLiang-Hangzhou <121126652qq@gmail.com>
2026-06-18 23:58:57 +0800

    chore: formatting and deps

--- api/quote.js
+++ api/quote.js
@@ -1,8 +1,11 @@
-
 const axios = require('axios');
 module.exports = async (req, res) => {
+    const { codes } = req.query;
     try {
-        const r = await axios.get("https://query1.finance.yahoo.com/v7/finance/quote?symbols=AAPL,MSFT,GOOG,AMZN,META,TSLA,NVDA,BABA,TCEHY");
+        const r = await axios.get(`http://push2.eastmoney.com/api/qt/ulist/get?secids=${codes}&fields=f12,f14,f2,f3,f4`, {
+            headers: { 'Referer': 'http://quote.eastmoney.com/' }
+        });
+        res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
         res.status(200).json(r.data);
     } catch (e) {
         res.status(500).json({ error: e.message });
